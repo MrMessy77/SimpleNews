7ccd264c2846dfa66b006372d5db340c6a83efed
MrMessy77 <313691730@qq.com>
2016-07-03 21:13:53 +0800

    添加第三方库

---
+++ SimpleNews/SimpleNews/News/Controller/NewsBaseViewController.h
@@ -0,0 +1,14 @@
+//
+//  NewsBaseViewController.h
+//  SimpleNews
+//
+//  Created by MrMessy on 16/6/29.
+//  Copyright © 2016年 MrMessy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#pragma mark - 新闻基础视图控制器
+@interface NewsBaseViewController : UIViewController
+
+@end
